1d97dc2d233734e3e9e436c27d246255e80af615
ccwijaya <candra.wijaya89@gmail.com>
2024-06-13 11:00:11 +0700

    update database

--- nma.sql
+++ nma.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version 5.5.5-10.1.8-MariaDB)
-# Date: 2024-06-02 22:46:32
+# Date: 2024-06-13 10:59:48
 # Generator: MySQL-Front 6.0  (Build 2.20)
 
 
@@ -63,13 +63,13 @@ CREATE TABLE `counter` (
   `update_user` int(11) DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 
 #
 # Data for table "counter"
 #
 
-INSERT INTO `counter` VALUES (1,'QUOTATION-2024-06-02',2,NULL,NULL,NULL,NULL);
+INSERT INTO `counter` VALUES (1,'QUOTATION-2024-06-02',2,NULL,NULL,NULL,NULL),(2,'NMA-Q2024-06-12',1,NULL,NULL,NULL,NULL);
 
 #
 # Structure for table "criticsuggestion"
@@ -197,20 +197,20 @@ DROP TABLE IF EXISTS `produk_jasa_detail`;
 CREATE TABLE `produk_jasa_detail` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `kode_jasa_detail` varchar(255) DEFAULT NULL,
-  `nama_jasa` varchar(255) DEFAULT NULL,
+  `nama_jasa` varchar(255) DEFAULT '',
   `keterangan` varchar(255) DEFAULT NULL,
   `create_user` int(11) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_user` int(11) DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 #
 # Data for table "produk_jasa_detail"
 #
 
-INSERT INTO `produk_jasa_detail` VALUES (1,'J001','20 FEET','Test 1234',NULL,NULL,1,'2024-06-02 16:26:55'),(2,'J002','40 FEET',NULL,NULL,NULL,NULL,NULL),(3,'J003','LCL 1 -5 Cbm',NULL,NULL,NULL,NULL,NULL),(4,'J004','( > Per Cbm )',NULL,NULL,NULL,NULL,NULL);
+INSERT INTO `produk_jasa_detail` VALUES (1,'J001','20 FEET','Test 1234',NULL,NULL,1,'2024-06-02 16:26:55'),(2,'J002','40 FEET',NULL,NULL,NULL,NULL,NULL),(3,'J003','LCL 1, 5 Cbm',NULL,NULL,NULL,NULL,NULL),(4,'J004','Per Cbm',NULL,NULL,NULL,NULL,NULL);
 
 #
 # Structure for table "sales"
@@ -268,28 +268,29 @@ CREATE TABLE `setting_email` (
 DROP TABLE IF EXISTS `sls_quotation`;
 CREATE TABLE `sls_quotation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `is_contract` int(11) NOT NULL DEFAULT '0',
-  `nama_sales` varchar(255) DEFAULT NULL,
-  `id_customer` int(11) DEFAULT NULL,
   `id_cabang` int(11) DEFAULT NULL,
   `no_bukti` varchar(255) DEFAULT NULL,
-  `no_contract` varchar(255) DEFAULT NULL,
   `tanggal` varchar(255) DEFAULT NULL,
-  `tanggal_contract` date DEFAULT NULL,
+  `id_customer` varchar(255) DEFAULT NULL,
+  `nama_sales` varchar(255) DEFAULT NULL,
   `keterangan` varchar(255) DEFAULT NULL,
   `id_produk_jasa` int(11) DEFAULT NULL,
+  `status_approve` int(11) DEFAULT NULL,
+  `nama_approved` varchar(255) DEFAULT NULL,
+  `waktu_approved` datetime DEFAULT NULL,
+  `ttd_approved` varchar(255) DEFAULT NULL,
   `create_user` int(11) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_user` int(11) DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 #
 # Data for table "sls_quotation"
 #
 
-INSERT INTO `sls_quotation` VALUES (1,0,'Santoso',1,1,'PJF-NMA-02-0624-002',NULL,'2024-06-02',NULL,'test1233454',1,1,'2024-06-02 17:34:15',1,'2024-06-02 17:43:59');
+INSERT INTO `sls_quotation` VALUES (2,1,'NMA-Q-12-0624-001','2024-06-12','1','Santoso','',2,NULL,NULL,NULL,NULL,1,'2024-06-12 18:01:59',1,'2024-06-12 18:27:21');
 
 #
 # Structure for table "sls_quotation_detail"
@@ -298,9 +299,42 @@ INSERT INTO `sls_quotation` VALUES (1,0,'Santoso',1,1,'PJF-NMA-02-0624-002',NULL
 DROP TABLE IF EXISTS `sls_quotation_detail`;
 CREATE TABLE `sls_quotation_detail` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_quotation` varchar(255) DEFAULT NULL,
+  `id_rr` int(11) DEFAULT NULL,
   `id_produk_jasa_detail` int(11) DEFAULT NULL,
   `harga` decimal(10,2) DEFAULT NULL,
+  `create_user` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_user` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+#
+# Data for table "sls_quotation_detail"
+#
+
+INSERT INTO `sls_quotation_detail` VALUES (27,2,2,54.00,1,'2024-06-12 18:27:21',NULL,NULL),(28,2,3,34.00,1,'2024-06-12 18:27:21',NULL,NULL),(29,2,1,567.00,1,'2024-06-12 18:27:21',NULL,NULL),(30,2,4,9090.00,1,'2024-06-12 18:27:21',NULL,NULL);
+
+#
+# Structure for table "sls_rate_request"
+#
+
+DROP TABLE IF EXISTS `sls_rate_request`;
+CREATE TABLE `sls_rate_request` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_paf` int(11) DEFAULT NULL,
+  `is_pre_project` int(11) DEFAULT NULL,
+  `is_dist` int(11) DEFAULT NULL,
+  `id_sales` varchar(255) DEFAULT NULL,
+  `id_cabang` int(11) DEFAULT NULL,
+  `no_bukti` varchar(255) DEFAULT NULL,
+  `tanggal` varchar(255) DEFAULT NULL,
+  `id_customer` varchar(255) DEFAULT NULL,
+  `keterangan` varchar(255) DEFAULT NULL,
+  `status_approve` int(11) DEFAULT NULL,
+  `nama_approved` varchar(255) DEFAULT NULL,
+  `waktu_approved` datetime DEFAULT NULL,
+  `ttd_approved` varchar(255) DEFAULT NULL,
   `create_user` int(11) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_user` int(11) DEFAULT NULL,
@@ -309,10 +343,85 @@ CREATE TABLE `sls_quotation_detail` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 #
-# Data for table "sls_quotation_detail"
+# Data for table "sls_rate_request"
 #
 
-INSERT INTO `sls_quotation_detail` VALUES (1,'1',1,123232.00,1,'2024-06-02 17:43:59',NULL,NULL);
+
+#
+# Structure for table "sls_rate_request_detail"
+#
+
+DROP TABLE IF EXISTS `sls_rate_request_detail`;
+CREATE TABLE `sls_rate_request_detail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_rr` int(11) DEFAULT NULL,
+  `id_rute` int(11) DEFAULT NULL,
+  `id_moda` int(11) DEFAULT NULL,
+  `id_kategori_kirim` int(11) DEFAULT NULL,
+  `jenis_muatan` varchar(255) DEFAULT NULL,
+  `pool_pm` varchar(255) DEFAULT NULL,
+  `km_pool_pm` decimal(10,2) DEFAULT NULL,
+  `km_lan_pm` decimal(10,2) DEFAULT NULL,
+  `total_km_pm` decimal(10,2) DEFAULT NULL,
+  `tempuh_hari_pm` decimal(10,2) DEFAULT NULL,
+  `hari_ops` decimal(10,2) DEFAULT NULL,
+  `total_hari_pm` decimal(10,2) DEFAULT NULL,
+  `ratio_pm` decimal(10,2) DEFAULT NULL,
+  `liter_pm` decimal(10,2) DEFAULT NULL,
+  `harga_ltr_pm` decimal(10,2) DEFAULT NULL,
+  `total_bbm_pm` decimal(10,2) DEFAULT NULL,
+  `uang_harian_supir_pm` decimal(10,2) DEFAULT NULL,
+  `lembur_supir_pm` decimal(10,2) DEFAULT NULL,
+  `total_umk_supir_pm` decimal(10,2) DEFAULT NULL,
+  `uang_harian_kenek_pm` decimal(10,2) DEFAULT NULL,
+  `lembur_kenek_pm` decimal(10,2) DEFAULT NULL,
+  `total_umk_kenek_pm` decimal(10,2) DEFAULT NULL,
+  `tol_pm` decimal(10,2) DEFAULT NULL,
+  `kapal_pm` decimal(10,2) DEFAULT NULL,
+  `bongkar_pm` decimal(10,2) DEFAULT NULL,
+  `muat_pm` decimal(10,2) DEFAULT NULL,
+  `mel_pm` decimal(10,2) DEFAULT NULL,
+  `internet_pm` decimal(10,2) DEFAULT NULL,
+  `total_ujp_pm` decimal(10,2) DEFAULT NULL,
+  `asuransi_pm` decimal(10,2) DEFAULT NULL,
+  `load_unload_pm` decimal(10,2) DEFAULT NULL,
+  `klaim_kerusakan_pm` decimal(10,2) DEFAULT NULL,
+  `spareparts_pm` decimal(10,2) DEFAULT NULL,
+  `pod_pm` decimal(10,2) DEFAULT NULL,
+  `total_varian_cost` decimal(10,2) DEFAULT NULL,
+  `fix_cost` decimal(10,2) DEFAULT NULL,
+  `test` varchar(255) DEFAULT NULL,
+  `total_cost` decimal(10,2) DEFAULT NULL,
+  `margin_15` decimal(10,2) DEFAULT NULL,
+  `margin_20` decimal(10,2) DEFAULT NULL,
+  `margin_30` decimal(10,2) DEFAULT NULL,
+  `margin_40` decimal(10,2) DEFAULT NULL,
+  `margin_50` decimal(10,2) DEFAULT NULL,
+  `tarif_approved` decimal(10,2) DEFAULT NULL,
+  `koreksi_tarif` decimal(10,2) DEFAULT NULL,
+  `volume_trip` decimal(10,2) DEFAULT NULL,
+  `tarif_sales` decimal(10,2) DEFAULT NULL,
+  `reff_sales` varchar(255) DEFAULT NULL,
+  `tarif_vendor` decimal(10,2) DEFAULT NULL,
+  `margin_vendor` decimal(10,2) DEFAULT NULL,
+  `persentase_vendor` decimal(10,2) DEFAULT NULL,
+  `reff_vendor` varchar(255) DEFAULT NULL,
+  `tarif_internal` decimal(10,2) DEFAULT NULL,
+  `reff_internal` varchar(255) DEFAULT NULL,
+  `tarif_platform` decimal(10,2) DEFAULT NULL,
+  `reff_platform` varchar(100) DEFAULT NULL,
+  `status_verif` varchar(255) DEFAULT NULL,
+  `create_user` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_user` int(11) DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+#
+# Data for table "sls_rate_request_detail"
+#
+
 
 #
 # Structure for table "status_verif"
@@ -376,13 +485,13 @@ CREATE TABLE `web_log` (
   `ip` varchar(100) DEFAULT '',
   `timing` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 #
 # Data for table "web_log"
 #
 
-INSERT INTO `web_log` VALUES (1,'admin','admin123',1,'','2024-04-30 11:28:39'),(2,'admin','admin123',1,'','2024-04-30 11:32:27'),(3,'admin','admin140313',0,'','2024-05-04 12:17:36'),(4,'admin','admin123',1,'','2024-05-04 12:17:39'),(5,'admin','admin123',1,'','2024-05-22 14:49:36'),(6,'candra','ccw140313',0,'','2024-06-02 15:56:09'),(7,'candra','ccw140313',0,'','2024-06-02 15:56:14');
+INSERT INTO `web_log` VALUES (1,'admin','admin123',1,'','2024-04-30 11:28:39'),(2,'admin','admin123',1,'','2024-04-30 11:32:27'),(3,'admin','admin140313',0,'','2024-05-04 12:17:36'),(4,'admin','admin123',1,'','2024-05-04 12:17:39'),(5,'admin','admin123',1,'','2024-05-22 14:49:36'),(6,'candra','ccw140313',0,'','2024-06-02 15:56:09'),(7,'candra','ccw140313',0,'','2024-06-02 15:56:14'),(8,'candra','ccw140313',0,'','2024-06-03 11:12:29'),(9,'admin','admin12',0,'','2024-06-03 11:39:01'),(10,'admin','admin123',1,'','2024-06-03 11:39:05'),(11,'admin','admin123',1,'','2024-06-11 12:08:52'),(12,'admin','admin123',1,'','2024-06-11 16:25:02'),(13,'admin','admin123',1,'','2024-06-12 09:48:34'),(14,'admin','admin123',1,'','2024-06-12 10:39:55'),(15,'admin','admin123',1,'','2024-06-12 10:46:08'),(16,'admin','admin123',1,'','2024-06-12 16:28:48'),(17,'admin','admin123',1,'','2024-06-12 16:39:52'),(18,'admin','admin123',1,'','2024-06-12 17:18:13'),(19,'admin','admin123',1,'','2024-06-12 17:54:24'),(20,'admin','admin123',1,'','2024-06-13 04:43:54');
 
 #
 # Structure for table "web_user"
@@ -451,7 +560,7 @@ CREATE TABLE `web_user` (
 # Data for table "web_user"
 #
 
-INSERT INTO `web_user` VALUES (1,'admin',NULL,NULL,2,4,'Logistic Analyst Spv',NULL,0,'Admin','0192023a7bbd73250516f069df18b500','081298868898','admin@gmail.com',NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,1,NULL,1,1,3,NULL,NULL,'avatar2.png','ttd.jpeg',NULL,NULL,0,1,'2024-05-22 14:49:36',1,NULL,'2022-12-19 02:14:00',1,'2024-05-22 14:49:36');
+INSERT INTO `web_user` VALUES (1,'admin',NULL,NULL,2,4,'Logistic Analyst Spv',NULL,0,'Admin','0192023a7bbd73250516f069df18b500','081298868898','admin@gmail.com',NULL,0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,NULL,NULL,1,NULL,1,1,3,NULL,NULL,'avatar2.png','ttd.jpeg',NULL,NULL,0,1,'2024-06-13 04:43:54',1,NULL,'2022-12-19 02:14:00',1,'2024-06-13 04:43:54');
 
 #
 # Structure for table "web_user_level"
@@ -490,7 +599,7 @@ CREATE TABLE `web_user_menu` (
   PRIMARY KEY (`id`),
   KEY `idx1` (`id_user`),
   KEY `idx2` (`id_menu`)
-) ENGINE=InnoDB AUTO_INCREMENT=4616 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=4617 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
 #
 # Data for table "web_user_menu"
@@ -514,39 +623,13 @@ CREATE TABLE `web_user_menu_list` (
   `update_user` int(11) DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
 #
 # Data for table "web_user_menu_list"
 #
 
 INSERT INTO `web_user_menu_list` VALUES (1,'user','Setting','User Setup',NULL,NULL,NULL,NULL,NULL),(2,'profil','Setting','Profile',NULL,NULL,NULL,NULL,NULL),(3,'cabang','Setting','Master Cabang',NULL,NULL,NULL,NULL,NULL),(4,'sales','Setting','Master Sales',NULL,NULL,NULL,NULL,NULL),(5,'customer','Sales','Master Customer Trucking',NULL,NULL,NULL,NULL,NULL),(6,'sls_quotation','Sales','Quotation',NULL,NULL,NULL,NULL,NULL),(7,'gnr_timeline','All Dept','Timeline Progress',NULL,NULL,NULL,NULL,NULL),(8,'gnr_timeline','All Dept','Time Line',NULL,NULL,NULL,NULL,NULL),(9,'gnr_timeline_received','Setting','Time Line Receiving',NULL,NULL,NULL,NULL,NULL),(10,'gnr_timeline_Approval','Approval','Time Line Approval',NULL,NULL,NULL,NULL,NULL),(11,'sls_contract','Sales','Contract',NULL,NULL,NULL,NULL,NULL),(12,'sls_quotation_unlock','Approval','Quotation Unlock',NULL,NULL,NULL,NULL,NULL),(13,'produk_jasa','Admin','Produk Jasa',NULL,NULL,NULL,NULL,NULL),(14,'produk_jasa_detail','Admin','Produk Jasa Detail',NULL,NULL,NULL,NULL,NULL);
-
-#
-# View "view_contract"
-#
-
-DROP VIEW IF EXISTS `view_contract`;
-CREATE
-  ALGORITHM = UNDEFINED
-  VIEW `view_contract`
-  AS
-SELECT
-  `a`.`id`,
-  `a`.`no_contract`,
-  `a`.`tanggal_contract`,
-  `b`.`id_rute`,
-  `c`.`rute`,
-  `d`.`unit` AS 'moda',
-  `c`.`tipe_box`,
-  `b`.`tarif_pbl`,
-  `b`.`tarif_nego`,
-  IF(`b`.`tarif_nego`, `b`.`tarif_nego`, `b`.`tarif_pbl`) AS 'tarif_contract'
-FROM
-  (((`sls_quotation` a
-    LEFT JOIN `sls_quotation_detail` b ON ((`a`.`id` = `b`.`id_quotation`)))
-    LEFT JOIN `view_rute` c ON ((`b`.`id_rute` = `c`.`id`)))
-    LEFT JOIN `moda` d ON ((`d`.`id` = `c`.`id_moda`)));
 
 #
 # View "view_customer"
@@ -562,47 +645,3 @@ SELECT
 FROM
   (`customer` a
     JOIN `sales` b ON ((`b`.`id` = `a`.`id_sales`)));
-
-#
-# View "view_quotation"
-#
-
-DROP VIEW IF EXISTS `view_quotation`;
-CREATE
-  ALGORITHM = UNDEFINED
-  VIEW `view_quotation`
-  AS
-SELECT
-  `a`.`id`,
-  `a`.`id_paf`,
-  `a`.`is_pre_project`,
-  `a`.`id_pre_quote`,
-  `a`.`is_distribution`,
-  `a`.`is_contract`,
-  `a`.`tc_moda`,
-  `a`.`id_sales`,
-  `a`.`id_cabang`,
-  `a`.`no_bukti`,
-  `a`.`no_contract`,
-  `a`.`cvp`,
-  `a`.`tanggal`,
-  `a`.`tanggal_contract`,
-  `a`.`id_customer`,
-  `a`.`keterangan`,
-  `a`.`status_proses_q`,
-  `a`.`status_approve_q`,
-  `a`.`waktu_approved_q`,
-  `a`.`nama_approved_q`,
-  `a`.`ttd_approved_q`,
-  `a`.`attach_quote_app`,
-  `a`.`attach_quote_old`,
-  `a`.`is_nego`,
-  `a`.`is_deal`,
-  `a`.`is_reject`,
-  `a`.`create_user`,
-  `a`.`create_time`,
-  `a`.`update_user`,
-  `a`.`update_time`,
-  (TO_DAYS(CURDATE()) - TO_DAYS(`a`.`create_time`)) AS 'selisih'
-FROM
-  `sls_quotation` a;
